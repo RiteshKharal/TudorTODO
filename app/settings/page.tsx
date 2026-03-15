eb9a4407d893afd6229a1af94766508e926ea4a8
Ritesh kharal <kharalritesh@gmail.com>
2026-03-15 18:26:33 +0545

    Separate settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import React from "react"
+import * as fonts from "@/app/font/fonts"
+import { IoIosArrowRoundBack } from "react-icons/io"
+import { ThemeToggle } from "../components/ThemeToggle"
+import Accmanager from "@/app/components/accmanager"
+import { IoSettings } from "react-icons/io5"
+import { useRouter } from "next/navigation"
+
+export default function Settings() {
+  const router = useRouter()
+
+  return (
+    <div className={`min-h-screen flex flex-col ${fonts.inconsolata.className} transition-transform`}>
+
+      <nav className="flex justify-between items-center px-10 py-6">
+
+        <div className={`text-2xl ${fonts.lilitaOne.className}`}>
+          Tudor
+        </div>
+
+        <div className="flex items-center gap-10">
+
+          <Accmanager cardtype="signup" />
+
+          <button onClick={() => router.back()}>
+            <IoSettings className="text-xl rotate-180 hover:scale-110 transition-all cursor-pointer" />
+          </button>
+
+        </div>
+
+      </nav>
+
+      <div className="flex justify-center items-center">
+
+        <div className="w-full backdrop-blur-md rounded-2xl p-8 ">
+
+          <button
+            className="flex items-center gap-2 mb-6 hover:-translate-x-1 transition-all ml-40 cursor-pointer"
+            onClick={() => router.back()}
+          >
+            <IoIosArrowRoundBack size={22} />
+            Back
+          </button>
+
+
+          <h1 className="text-3xl font-bold mb-10 text-center">
+            Settings
+          </h1>
+
+
+          <div className="flex flex-col gap-6">
+
+            <div className="flex items-center justify-evenly p-4 rounded-xl">
+
+              <span className="text-lg font-medium">
+                Theme
+              </span>
+
+              <ThemeToggle />
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
