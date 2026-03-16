b5fd4376b813add60561a1d7093f6e0096979d86
Ritesh kharal <kharalritesh@gmail.com>
2026-03-16 18:14:59 +0545

    Production fix

--- app/page.tsx
+++ app/page.tsx
@@ -12,22 +12,26 @@ export const dynamic = "force-dynamic";
 export default function Home() {
   return (
     <div className="flex items-center justify-center  font-sans ">
-      <main className="flex w-full max-w-3xl flex-col items-center justify-between p-10  sm:items-start">
-        <nav className="top-10 flex justify-between w-full animate-[StretchIn_0.2s_ease-in]">
+      <main className="flex w-full max-w-3xl flex-col items-center justify-between p-10  sm:items-start ">
+        <nav className="top-10 flex justify-between w-full animate-[StretchIn_0.2s_ease-in] ">
           <div className={`text-xl ${fonts.lilitaOne.className} `}>Tudor</div>
 
           <div>
-            <div className="flex flex-row gap-5 text-center justify-center">
+            <div className="flex flex-row gap-5 text-center justify-center ">
               <Accmanager cardtype="signup" />
 
               <div>
                 <a href="/settings">
-                  <IoSettings cursor="pointer" className=" text-xl hover:animate-[Rotate180_1s_infinite]" />
+                  <IoSettings cursor="pointer" className=" text-xl hover:animate-[Rotate180_1s_infinite] " />
                 </a>
               </div>
             </div>
           </div>
         </nav>
+
+
+
+
 
         <section className=" w-full mt-[5%] ">
           <Task />
