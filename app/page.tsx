230c1d2ab845524b477b30d136c3fed8e49c6d08
Ritesh kharal <kharalritesh@gmail.com>
2026-03-16 18:13:55 +0545

    Made minor improvements by addind subtle animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,38 @@
 import Image from "next/image";
-import {ThemeToggle} from './components/ThemeToggle'
-import * as fonts from './font/fonts'
-import Settings from './components/Settings';
+import { ThemeToggle } from "./components/ThemeToggle";
+import * as fonts from "./font/fonts";
+import Settings from "./components/Settings";
 import Accmanager from "./components/accmanager";
 import TaskManager, { UserTasks } from "./backend/TaskManager";
 import { Task } from "./components/TaskCard";
 import { IoSettings } from "react-icons/io5";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export default function Home() {
-
   return (
-    <div className="flex items-center justify-center  font-sans">
+    <div className="flex items-center justify-center  font-sans ">
       <main className="flex w-full max-w-3xl flex-col items-center justify-between p-10  sm:items-start">
-
-        <nav className="top-10 flex justify-between w-full ">
-
-          <div className={`text-xl ${fonts.lilitaOne.className}`} >
-            Tudor
-          </div>
+        <nav className="top-10 flex justify-between w-full animate-[StretchIn_0.2s_ease-in]">
+          <div className={`text-xl ${fonts.lilitaOne.className} `}>Tudor</div>
 
           <div>
+            <div className="flex flex-row gap-5 text-center justify-center">
+              <Accmanager cardtype="signup" />
 
-          <div className="flex flex-row gap-5 text-center justify-center">
-            <Accmanager cardtype="signup"/>
-            
-            <div>
-              <a href="/settings">
-              <IoSettings
-                      cursor="pointer"
-                      className=" text-xl"
-                    />
-              </a>
+              <div>
+                <a href="/settings">
+                  <IoSettings cursor="pointer" className=" text-xl hover:animate-[Rotate180_1s_infinite]" />
+                </a>
+              </div>
             </div>
-            
-            </div>
-         </div>
+          </div>
         </nav>
 
         <section className=" w-full mt-[5%] ">
-      
-
-      <Task/>
+          <Task />
         </section>
       </main>
     </div>
   );
 }
-
